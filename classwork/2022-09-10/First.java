26debd0eab3858e8d1403eb4b151adc994f86951
Nice77 <n.i.y.a.z.19831977@gmail.com>
2022-09-10 10:51:11 +0300

    Edited first task, added second

--- classwork/2022-09-10/First.java
+++ classwork/2022-09-10/First.java
@@ -1,9 +1,5 @@
-public class Name {
+public class First {
     public static void main(String[] args) {
-        System.out.println("1  1  1  1   111   11 ");
-	System.out.println("1  1  1  1  1  1  1  1");
-	System.out.println("1111  1 11   111    1 ");
-	System.out.println("1  1  11 1   1 1  1  1");
-	System.out.println("1  1  1  1  1  1   11 ");
+        System.out.println("1  1  1  1   111   11 \n1  1  1  1  1  1  1  1\n1111  1 11   111    1 \n1  1  11 1   1 1  1  1\n1  1  1  1  1  1   11 \n");
     }
 }
---
+++ classwork/2022-09-10/Second.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Second {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+	/*
+        int a = scanner.nextInt();
+	int b = scanner.nextInt();
+	System.out.println(a + b);
+	*/
+	System.out.println(scanner.nextInt() + scanner.nextInt());
+    }
+}
