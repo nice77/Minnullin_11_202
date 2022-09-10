15da8003c81549e7d2f58b66fceb9823d4631756
Nice77 <n.i.y.a.z.19831977@gmail.com>
2022-09-10 11:36:22 +0300

    Edited files

--- classwork/2022-09-10/Task02.java
+++ classwork/2022-09-10/Task02.java
@@ -2,12 +2,17 @@ import java.util.Scanner;
 
 public class Task02 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-	/*
-        int a = scanner.nextInt();
-	int b = scanner.nextInt();
-	System.out.println(a + b);
-	*/
-	System.out.println(scanner.nextInt() + scanner.nextInt());
+        Scanner sc = new Scanner(System.in);
+	float a = sc.nextFloat();
+	float b = sc.nextFloat();
+	if (a > b) {
+	    System.out.println("First number larger");
+	}
+	else if (b > a) {
+	    System.out.println("Second number larger");
+	}
+	else {
+	    System.out.println("Numbers are equal");
+	}
     }
 }
