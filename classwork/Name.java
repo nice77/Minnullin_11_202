f6dee4cedc909a8e3dd9970a66c65d5ca1e7738c
Nice77 <n.i.y.a.z.19831977@gmail.com>
2022-09-10 10:32:35 +0300

    First task completed

---
+++ classwork/Name.java
@@ -0,0 +1,9 @@
+public class Name {
+    public static void main(String[] args) {
+        System.out.println("1  1  1  1   111   11 ");
+	System.out.println("1  1  1  1  1  1  1  1");
+	System.out.println("1111  1 11   111    1 ");
+	System.out.println("1  1  11 1   1 1  1  1");
+	System.out.println("1  1  1  1  1  1   11 ");
+    }
+}
