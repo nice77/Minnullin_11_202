2d4a03f27e4ac1da63c9d33a847b89d9d2534fa3
Nice77 <n.i.y.a.z.19831977@gmail.com>
2022-09-10 13:23:45 +0300

    Solved task 09

---
+++ classwork/2022-09-10/Task09.java
@@ -0,0 +1,8 @@
+public class Task09 {
+    public static void main(String[] args) {
+	int k = Integer.parseInt(args[0]);
+	for (int i = 1; i <= k; i++) {
+	    System.out.println(i + " * " + k + " = " + i * k);
+	}
+    }
+}
