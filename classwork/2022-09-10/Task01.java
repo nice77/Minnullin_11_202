b427b5371af501a06e29a981b95e278e50867dd6
Nice77 <n.i.y.a.z.19831977@gmail.com>
2022-09-10 10:55:52 +0300

    renamed files

--- classwork/2022-09-10/Task01.java
+++ classwork/2022-09-10/Task01.java
@@ -1,4 +1,4 @@
-public class First {
+public class Task01 {
     public static void main(String[] args) {
         System.out.println("1  1  1  1   111   11 \n1  1  1  1  1  1  1  1\n1111  1 11   111    1 \n1  1  11 1   1 1  1  1\n1  1  1  1  1  1   11 \n");
     }
--- classwork/2022-09-10/Task02.java
+++ classwork/2022-09-10/Task02.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Second {
+public class Task02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 	/*
