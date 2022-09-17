86a8f2e06619cffac6ff0e2c812747f79b981989
Nice77 <n.i.y.a.z.19831977@gmail.com>
2022-09-17 10:58:41 +0300

    Solved task 01

---
+++ classwork/2022-09-17/Task01.java
@@ -0,0 +1,10 @@
+public class Task01 {
+    public static void main(String[] args) {
+	for (int i = 2; i < 10; i++) {
+	    for (int j = 2; j < 10; j++) {
+		System.out.print(j + " * " + i + " = " + (i * j) + "\t");
+	    }
+	    System.out.println();
+	}
+    }
+}
