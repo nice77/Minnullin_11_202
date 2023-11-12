a59da0d560b505374eee1491d43bc2936e0c3377
nice77 <n.i.y.a.z.19831977@gmail.com>
2023-11-12 15:19:08 +0300

    Added adaptive menu button logic

--- 3s/semester/src/main/webapp/resources/js/script-main.js
+++ 3s/semester/src/main/webapp/resources/js/script-main.js
@@ -9,14 +9,23 @@ const start = () => {
 start()
 
 const fade = (path) => {
-    gsap.to('.main-page__element > *', {
+    const timeline = gsap.timeline()
+
+    const fadeAnim = gsap.to('.main-page__element > *', {
         opacity: 0,
         duration: 0.3,
         stagger: 0.01,
         onComplete: () => {
             window.location.href = path
         }
+    }, ">")
+
+    const backMenu = gsap.to('.main-page__menu', {
+        top: '-100px',
+        duration: 0.3
     })
+
+    timeline.to(backMenu).to(fadeAnim)
 }
 
 document.getElementById('profile').onclick = (event) => {
@@ -30,3 +39,44 @@ document.getElementById('vacancies').onclick = (event) => {
 document.getElementById('settings').onclick = (event) => {
     fade("./settings")
 }
+
+
+const toX = () => {
+    const timeline = gsap.timeline()
+    const uno = gsap.to("#expand_button span:nth-child(1)", {
+        transform: "translateY(8px)",
+        rotation: 45
+    })
+    const dos = gsap.to("#expand_button span:nth-child(3)", {
+        transform: "translateY(-8px)",
+        rotation: 45
+    })
+    const tres = gsap.to("#expand_button span:nth-child(2)", {
+        rotation: -45
+    })
+    $(".main-page__menu").css({"top": 5 + $("header").height() + "px"})
+    timeline.to(uno).to(dos).to(tres)
+}
+
+const toBurger = () => {
+    const timeline = gsap.timeline()
+    const uno = gsap.to("#expand_button span:nth-child(1)", {
+        transform: "inherit"
+    })
+    const dos = gsap.to("#expand_button span:nth-child(2)", {
+        rotation: 0
+    })
+    const tres = gsap.to("#expand_button span:nth-child(3)", {
+        transform: "inherit"
+    })
+    $(".main-page__menu").css({"top": "-100px"})
+    timeline.to(uno).to(dos).to(tres)
+}
+
+let opened = false
+$(document).ready(() => {
+    $("#expand_button").on("click", () => {
+        opened = !opened
+        opened ? toX() : toBurger()
+    })
+})
