7f61b2ed13d088480b8a4aaf764a93a97d433588
nice77 <n.i.y.a.z.19831977@gmail.com>
2023-11-15 12:15:25 +0300

    Redundant file removed

--- 3s/semester/src/main/webapp/resources/js/main-page/script-company.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('createNewBtn').onclick = (event) => {
-    fade("./create")
-}
-gsap.utils.toArray(".card").forEach(item => {
-    const element = item.querySelector(".vacancy")
-    element.onclick = (event) => {
-        fade("./vacancies?vid=" + element.id)
-    }
-})
