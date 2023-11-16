b3268d93be3dbc9e1768581d67b328cdee9efe04
nice77 <n.i.y.a.z.19831977@gmail.com>
2023-11-16 23:00:40 +0300

    JS logic of creating posts page rewritten using JQuery

--- 3s/semester/src/main/webapp/resources/js/createPost/script.js
+++ 3s/semester/src/main/webapp/resources/js/createPost/script.js
@@ -1,32 +1,34 @@
-document.getElementById("clickToUpload").onclick = (event) => {
-    document.getElementById("inputFileUploader").click()
-}
+$(document).ready(() => {
+    $("#clickToUpload").on("click", (event) => {
+        $("#inputFileUploader").click()
+    })
 
-let edited = false
-document.getElementById("bodyInput").oninput = (event) => {
-    if (!edited) {
-        edited = true
-        const elem = document.getElementById("bodyInput")
-        elem.innerText = event.target.innerText.substring(event.target.innerText.length - 1)
+    let edited = false
+    $("#bodyInput").on("input", (event) => {
+        if (!edited  && document.querySelector("#hiddenInputField").value === "") {
+            edited = true
+            const elem = document.getElementById("bodyInput")
+            elem.innerText = event.target.innerText.substring(event.target.innerText.length - 1)
 
-        if(document.createRange)
-        {
-            range = document.createRange()
-            range.selectNodeContents(elem)
-            range.collapse(false)
-            selection = window.getSelection()
-            selection.removeAllRanges()
-            selection.addRange(range)
+            if(document.createRange)
+            {
+                range = document.createRange()
+                range.selectNodeContents(elem)
+                range.collapse(false)
+                selection = window.getSelection()
+                selection.removeAllRanges()
+                selection.addRange(range)
+            }
+            else if(document.selection)
+            {
+                range = document.body.createTextRange()
+                range.moveToElementText(elem)
+                range.collapse(false)
+                range.select()
+            }
         }
-        else if(document.selection)
-        {
-            range = document.body.createTextRange()
-            range.moveToElementText(elem)
-            range.collapse(false)
-            range.select()
-        }
-    }
 
-    const bodyInputField = document.getElementById("hiddenInputField")
-    bodyInputField.value = document.getElementById("bodyInput").innerText
-}
+        const bodyInputField = document.getElementById("hiddenInputField")
+        bodyInputField.value = document.getElementById("bodyInput").innerText
+    })
+})
