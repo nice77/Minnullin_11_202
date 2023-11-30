fe6ec6324528cdd40bb56750c1c5f0c498fffba7
nice77 <n.i.y.a.z.19831977@gmail.com>
2023-11-30 12:12:54 +0300

    Redundant lines removed

--- 3s/semester2/src/client/Main.java
+++ 3s/semester2/src/client/Main.java
@@ -12,6 +12,10 @@ public class Main extends Application {
 
     private Parent menuSceneRoot;
     private Scene menuScene;
+    private Parent selectSceneRoot;
+    private Scene selectScene;
+    private Parent fightSceneRoot;
+    private Scene fightScene;
 
     public static void main(String[] args) {
         launch(args);
@@ -20,15 +24,29 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         this.menuSceneRoot = FXMLLoader.load(getClass().getResource(Resources.START_SCREEN));
-        this.menuScene.getStylesheets().add(Resources.START_SCREEN_STYLE);
-        System.out.println(KeyCode.ENTER);
+        this.menuScene = new Scene(this.menuSceneRoot);
+        this.selectSceneRoot = FXMLLoader.load(getClass().getResource(Resources.CHARACTER_SELECT_SCREEN));
+        this.selectScene = new Scene(this.selectSceneRoot);
+        this.fightSceneRoot = FXMLLoader.load(getClass().getResource(Resources.FIGHT_SCREEN));
+        this.fightScene = new Scene(this.fightSceneRoot);
+
+
+        this.menuScene.getStylesheets().add(Resources.WINDOW_STYLE);
         this.menuScene.setOnKeyPressed(event -> {
-            // switching scene logic
+            if (event.getCode() == KeyCode.ENTER) {
+                primaryStage.setScene(this.selectScene);
+            }
         });
 
-        System.out.println(Font.loadFont(
+        this.selectScene.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.ENTER) {
+                primaryStage.setScene(this.fightScene);
+            }
+        });
+
+        Font.loadFont(
                 getClass().getResource(Resources.DPCOMIC_FONT).toExternalForm(),
-                Resources.DPCOMIC_FONT_SIZE));
+                Resources.DPCOMIC_FONT_SIZE);
 
         primaryStage.setTitle(Resources.WINDOW_TITLE);
         primaryStage.setScene(this.menuScene);
