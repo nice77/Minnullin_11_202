96b4bc08fd6adcbd795ea783c7a5aa1e1992c81d
nice77 <n.i.y.a.z.19831977@gmail.com>
2023-11-30 12:13:47 +0300

    Modified resources strings due to multiple replacements

--- 3s/semester2/src/client/Resources.java
+++ 3s/semester2/src/client/Resources.java
@@ -4,6 +4,7 @@ public class Resources {
     public static final String WINDOW_TITLE = "Mortal Kombat";
     public static final int WINDOW_WIDTH = 800;
     public static final int WINDOW_HEIGHT = 600;
+    public static final String WINDOW_STYLE = "/client/assets/style.css";
 
 
 
@@ -11,7 +12,22 @@ public class Resources {
     public static final int DPCOMIC_FONT_SIZE = 20;
 
 
-    public static final String START_SCREEN = "/client/view/start.fxml";
-    public static final String START_SCREEN_STYLE = "/client/view/style.css";
-    public static final String START_SCREEN_IMAGE = "/client/assets/title.png";
+
+    public static final String START_SCREEN = "/client/assets/startScreen/start.fxml";
+    public static final String START_SCREEN_IMAGE = "/client/assets/startScreen/title.png";
+
+
+
+    public static final String CHARACTER_SELECT_SCREEN = "/client/assets/characterSelectScreen/characterSelect.fxml";
+    public static final String CHARACTER_SELECT_SCREEN_IMAGE = "/client/assets/characterSelectScreen/characterSelect.jpg";
+    public static final int CHARACTER_IMAGE_WIDTH = 116;
+    public static final int CHARACTER_IMAGE_HEIGHT = 175;
+    public static final int CHARACTER_IMAGE_GAP = 18;
+    public static final int CHARACTER_IMAGE_RECT_STROKE_WIDTH = 5;
+
+
+
+    public static final String FIGHT_SCREEN = "/client/assets/fightScreen/fightScreen.fxml";
+    public static final String ARENA_01 = "/client/assets/fightScreen/arena01.png";
+
 }
