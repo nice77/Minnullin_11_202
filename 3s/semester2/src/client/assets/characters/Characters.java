68d577fbd1b4300bfaeb0526ff9e038042d48ddd
nice77 <n.i.y.a.z.19831977@gmail.com>
2023-12-10 22:01:16 +0300

    Updated screens' assets

--- 3s/semester2/src/client/assets/characters/Characters.java
+++ 3s/semester2/src/client/assets/characters/Characters.java
@@ -8,21 +8,27 @@ public enum Characters {
             "/client/assets/characters/sprites/scorpion.png",
             new HashMap<String, int[]>() {{
                 // width, height, offsetX, offsetY, count
-                put("idle", new int[] {53, 110, 77, 17, 6});
+                put("IDLE", new int[] {53, 110, 77, 17, 6});
+                put("WALK", new int[] {50, 110, 10, 130, 9});
+                put("BLOCK", new int[] {48, 108, 514, 17, 1});
             }}
     ),
     SUB_ZERO(
             "/client/assets/characters/sprites/sub-zero.png",
             new HashMap<String, int[]>() {{
                 // width, height, offsetX, offsetY, count
-                put("idle", new int[] {(int) (46 * 1.5) + 1, 150, (int) (74 * 1.5) - 5, 30, 10});
+                put("IDLE", new int[] {70, 150, 106, 30, 10});
+                put("WALK", new int[] {75, 162, 25, 200, 9});
+                put("BLOCK", new int[] {77, 156, 975, 25, 1});
             }}
     ),
     JOHNNY_CAGE(
             "client/assets/characters/sprites/johnny_cage.png",
             new HashMap<String, int[]>() {{
                 // width, height, offsetX, offsetY, count
-                put("idle", new int[] {67, 105, 110, 20, 7});
+                put("IDLE", new int[] {67, 105, 110, 20, 7});
+                put("WALK", new int[] {60, 106, 25, 135, 9});
+                put("BLOCK", new int[] {62, 104, 613, 17, 1});
             }}
 
     ),
@@ -30,28 +36,36 @@ public enum Characters {
             "client/assets/characters/sprites/kano.png",
             new HashMap<String, int[]>() {{
                 // width, height, offsetX, offsetY, count
-                put("idle", new int[] {54, 104, 81, 25, 7});
+                put("IDLE", new int[] {54, 104, 81, 25, 7});
+                put("WALK", new int[] {55, 103, 20, 153, 9});
+                put("BLOCK", new int[] {48, 110, 476, 17, 1});
             }}
     ),
     RAIDEN(
             "client/assets/characters/sprites/raiden.png",
             new HashMap<String, int[]>() {{
                 // width, height, offsetX, offsetY, count
-                put("idle", new int[] {59, 110, 97, 20, 10});
+                put("IDLE", new int[] {59, 110, 97, 20, 10});
+                put("WALK", new int[] {52, 105, 780, 142, 9});
+                put("BLOCK", new int[] {64, 110, 12, 140, 1});
             }}
     ),
     LIU_KANG(
             "client/assets/characters/sprites/liu-kang.png",
             new HashMap<String, int[]>() {{
                 // width, height, offsetX, offsetY, count
-                put("idle", new int[] {57, 98, 89, 18, 8});
+                put("IDLE", new int[] {57, 98, 89, 18, 8});
+                put("WALK", new int[] {45, 105, 19, 145, 9});
+                put("BLOCK", new int[] {66, 102, 773, 18, 1});
             }}
     ),
     SONYA_BLADE(
             "client/assets/characters/sprites/sonya-blade.png",
             new HashMap<String, int[]>() {{
                 // width, height, offsetX, offsetY, count
-                put("idle", new int[] {46, 105, 76, 15, 7});
+                put("IDLE", new int[] {46, 105, 76, 15, 7});
+                put("WALK", new int[] {46, 110, 20, 140, 9});
+                put("BLOCK", new int[] {52, 102, 586, 20, 1});
             }}
     );
 
