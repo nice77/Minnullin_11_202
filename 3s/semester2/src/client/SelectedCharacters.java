404f71fba95a281a79e7f8cfe8425ad7ad282666
nice77 <n.i.y.a.z.19831977@gmail.com>
2023-12-10 22:02:30 +0300

    Added global settings of selected and playable character

---
+++ 3s/semester2/src/client/SelectedCharacters.java
@@ -0,0 +1,10 @@
+package client;
+
+import client.controller.fightScreen.Player;
+
+
+public class SelectedCharacters {
+    public static Player mainPlayer = null;
+    public static Player otherPlayer = null;
+    public static Player playable = null;
+}
